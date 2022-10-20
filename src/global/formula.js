4128ae2866d1c734baa347f83d7af683cfd8c449
bc4s <73247940+bc4s@users.noreply.github.com>
2022-10-20 11:43:36 +0200

    Semicolon separator in functions (#15)
    * semicolon separator
    
    * comma
    
    * semicilon separator in useCommaDecimalSeparator ff
    
    * semicolon sep
    
    Co-authored-by: Branislav Caban <branislav.caban@kros.sk>

--- src/global/formula.js
+++ src/global/formula.js
@@ -1075,7 +1075,7 @@ const luckysheetformula = {
                 nameli = paramitem.name;
 
             if (paramitem.repeat == "y") {
-                name += ", ...";
+                name += `${this.getFunctionSeparator()} ...`;
                 nameli += '<span class="luckysheet-arguments-help-argument-info">...-' + locale_formulaMore.allowRepeatText + '</span>';
             }
             if (paramitem.require == "o") {
@@ -1083,8 +1083,8 @@ const luckysheetformula = {
                 nameli += '<span class="luckysheet-arguments-help-argument-info">-[' + locale_formulaMore.allowOptionText + ']</span>';
             }
 
-            fht += '<span class="luckysheet-arguments-help-parameter" dir="auto">' + name + '</span>, ';
-            ahf += '<span class="luckysheet-arguments-help-parameter" dir="auto">' + paramitem.example + '</span>, ';
+            fht += '<span class="luckysheet-arguments-help-parameter" dir="auto">' + name + `</span> ${this.getFunctionSeparator()} `;
+            ahf += '<span class="luckysheet-arguments-help-parameter" dir="auto">' + paramitem.example +`</span>${this.getFunctionSeparator()} `;
             fhcp += replaceHtml(helpformulaArg, {
                 "param": nameli,
                 "content": paramitem.detail
@@ -3524,9 +3524,9 @@ const luckysheetformula = {
                 str += "'";
                 matchConfig.squote = matchConfig.squote == 0 ? 1 : 0;
             }
-            else if (s == ',' && matchConfig.squote == 0 && matchConfig.dquote == 0 && matchConfig.braces == 0) {
+            else if (s == this.getFunctionSeparator() && matchConfig.squote == 0 && matchConfig.dquote == 0 && matchConfig.braces == 0) {
                 //matchConfig.comma += 1;
-                function_str += _this.functionHTML(str) + '<span dir="auto" class="luckysheet-formula-text-comma">,</span>';
+                function_str += _this.functionHTML(str) + `<span dir="auto" class="luckysheet-formula-text-comma">${this.getFunctionSeparator()}</span>`;
                 str = "";
             }
             else if (s == '&' && matchConfig.squote == 0 && matchConfig.dquote == 0 && matchConfig.braces == 0) {
@@ -3619,6 +3619,13 @@ const luckysheetformula = {
         }
 
         return function_str;
+    },
+    getFunctionSeparator: function() {
+        if (luckysheetConfigsetting.useCommaDecimalSeparator) {
+            return ";";
+        } else {
+            return ",";
+        }
     },
     getfunctionParam: function (txt) {
         let _this = this;
@@ -3849,10 +3856,8 @@ const luckysheetformula = {
             return "";
         }
 
-        let separator = ',';
         if (luckysheetConfigsetting.useCommaDecimalSeparator) {
             txt = txt.replaceAll(',', '.');
-            separator = '.';
         }
 
         if (txt.substr(0, 2) == "=+") {
@@ -3992,7 +3997,7 @@ const luckysheetformula = {
                     firstSQ = i;
                 }
             }
-            else if (s == separator && bracket.length == 1 && matchConfig.squote == 0 && matchConfig.dquote == 0 && matchConfig.braces == 0) {
+            else if (s == this.getFunctionSeparator() && matchConfig.squote == 0 && matchConfig.dquote == 0 && matchConfig.braces == 0) {
                 if (bracket.length <= 1) {
                     let functionS = _this.functionParser(str, cellRangeFunction);
                     if (functionS.indexOf("#lucky#") > -1) {
