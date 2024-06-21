58f21345c30e662cba82e28a3e38aea462303aea
Peter Hrasko <hrasko@kros.sk>
2024-06-21 10:04:23 +0200

    czech language

--- src/locale/locale.js
+++ src/locale/locale.js
@@ -2,10 +2,11 @@ import en from './en'
 import zh from './zh'
 import es from './es'
 import sk from './sk'
+import cz from './cz'
 import zh_tw from './zh_tw'
 import Store from '../store';
 
-export const locales = {en,zh,es,sk,zh_tw}
+export const locales = {en,zh,es,sk,cz,zh_tw}
 
 function locale(){
     return locales[Store.lang];
